0d72fe0ff3e44600d351151ab96435695a413491
Shashvat Patel <shashvatpatel15@gmail.com>
2026-05-28 23:52:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
+import express from "express";
+import cors from "cors";
 
+import dotenv from "dotenv";
+dotenv.config()
+import connectDB from "./config/db.js";
+connectDB()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json());
+app.use(cors({credentials:true}));
+
+import authRouter from "./routes/authRoutes.js";
+
+// API Endpoints
+app.use('/api/auth',authRouter);
+
+app.listen(port,() => {
+  console.log(`app is running on port ${port}`)
+})
