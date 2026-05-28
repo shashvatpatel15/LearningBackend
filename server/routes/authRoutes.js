c93748e4d490c82d959a0e94150e7156f0a0b200
Shashvat Patel <shashvatpatel15@gmail.com>
2026-05-28 23:53:12 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,1 +1,11 @@
+import express from "express"
+import authController from "../controller/authController.js"
 
+const authRouter = express.Router();
+
+authRouter.post('/register', authController.register);
+authRouter.post('/login', authController.login);
+authRouter.post('/logout', authController.logout);
+
+     
+export default authRouter;
